99c297164c3e3b70657127dc8a74190e4dc0046b
Rodrigo Santos <119634097+xRodrigoSx@users.noreply.github.com>
2024-06-17 21:50:21 -0300

    Add files via upload

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import React, { FC } from "react";
 import { Pokemons } from "../types/response";
 import { useNavigation } from "@react-navigation/native";
@@ -12,14 +12,16 @@ const Card: FC<CardProps> = ({ item }) => {
 
   const goToDetails = () => {
     if (navigation) {
-    navigation.navigate("Details", item);
-  } else {
-    console.log("Navigation object is undefined");
-  }
-};
+      navigation.navigate("Details", item);
+    } else {
+      console.log("Navigation object is undefined");
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.id}>{"#"+item.order}</Text>
+      <Image source={{ uri: item.sprites.back_default }} style={styles.avatar} />
       <Text style={styles.title}>{item.name.toUpperCase()}</Text>
       <TouchableOpacity style={styles.button} onPress={goToDetails}>
         <Text style={styles.buttonText}>Detalhes</Text>
@@ -32,27 +34,38 @@ export default Card;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
     marginVertical: 10,
     backgroundColor: "#a1b2c3d4",
     marginHorizontal: 10,
+    borderRadius: 5,
+  },
+  id: {
+    fontSize: 20,
+    color: "#333",
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    backgroundColor: "gray",
   },
   title: {
-    fontSize: 22,
+    fontSize: 16,
   },
   description: {
     fontSize: 18,
   },
   button: {
     marginVertical: 10,
-    width: "40%",
+    width: "85%",
     height: 30,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#252525",
+    borderRadius: 5,
   },
   buttonText: {
     color: "#fff",
